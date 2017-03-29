43a88655f7d3b5df0fbcc462618114f3cde89f9d
Serafin <leaf466@gmail.com>
2017-03-29 11:15:39 -0700

    Adds ajax request to Postalicious server from client side js

--- postalicious/app.js
+++ postalicious/app.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const httpRequest = require('request')
 const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (request, response) => {
   response.sendFile('index.html')
+})
+
+app.post('/ajax', (request, response) => {
+  let body = request.body
+  console.log('body', body)
+  response.send('I finished')
 })
 
 app.listen(3001)
--- postalicious/public/main.js
+++ postalicious/public/main.js
@@ -1,24 +1,29 @@
 'use strict'
 
+console.log('I\'m loading')
+
 function grabDom() {
   let values = document.querySelectorAll('.inputFields *')
   let filteredValues = []
   values.forEach( element => {
     if( element.tagName === "INPUT" || element.tagName === "TEXTAREA" ) {
-      filteredValues.push({
-        'id': element.id,
-        'value': element.value
-      })
+      filteredValues.push(element.value)
     }
   })
-  let options = {
+
+  // TODO: Javascript error handling BEFORE the send button is clicked
+  // event handlers on loseFocus for each input field
+
+  filteredValues[0] = filteredValues[0] ? filteredValues[0].toUpperCase() : 'GET'
+
+  return {
     'url': filteredValues[1],
     'qs': {
       [filteredValues[2]]: filteredValues[5],
       [filteredValues[3]]: filteredValues[6],
       [filteredValues[4]]: filteredValues[7]
     },
-    'method': filteredValues[0].toUpperCase(),
+    'method': filteredValues[0],
     'headers': {
       [filteredValues[8]]: filteredValues[11],
       [filteredValues[9]]: filteredValues[12],
@@ -26,4 +31,17 @@ function grabDom() {
     },
     'body': filteredValues[14]
   }
+}
+
+function ajax() {
+  const options = grabDom()
+  fetch('//localhost:3001/ajax', {  
+    method: 'post', 
+    body: options // TODO Im not working, help me!
+  })
+  .then( response => {
+    console.log('client side: ', response)
+  }).catch( error => {
+    console.error('Oh no, you ', error, '\'d')
+  })
 }
